ada3972d9ee81b700244634507c356296d5ab389
Qarib Iqbal <qaribiqbal92@gmail.com>
2024-12-20 20:49:25 +0500

    Fetching users successfully

--- credit_app/lib/main.dart
+++ credit_app/lib/main.dart
@@ -1,35 +1,3 @@
-// import 'package:credit_app/add_card_screen.dart';
-// import 'package:credit_app/card_list_screen.dart';
-// import 'package:firebase_core/firebase_core.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
-// //import 'package:firebase_core/firebase_core.dart';
-// import 'package:flutter/material.dart';
-// import 'firebase_options.dart';
-// import 'package:credit_app/login_screen.dart';
-// import 'dart:convert';
-// import 'package:http/http.dart' as http;
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Credit App',
-//       theme: ThemeData(
-//         primarySwatch: Colors.blue,
-//       ),
-//       debugShowCheckedModeBanner: false,
-//       home: //const UserDetailsPage(),
-//             const LoginScreen(),
-//     );
-//   }
-// }
 import 'package:credit_app/add_card_screen.dart';
 import 'package:credit_app/card_list_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
